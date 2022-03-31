902fd8c793915832fa91e06b8a7fd578e502a738
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2022-03-31 10:54:26 +0300

    create mock guard

--- client/src/app/services/mock-auth.guard.ts
+++ client/src/app/services/mock-auth.guard.ts
@@ -1,17 +1,15 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
-import { AuthService } from "../services/auth.service";
 
 @Injectable({
     providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class MockAuthGuard implements CanActivate {
 
     /**
      *
      */
-    constructor(private authService: AuthService, private router: Router) {
-
+    constructor(private router: Router) {
 
     }
 
