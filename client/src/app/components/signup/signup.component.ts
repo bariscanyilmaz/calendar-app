062f940f07ee8c078992b769ec11cea8a2622bf8
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2022-03-31 11:55:20 +0300

    create signup page

--- client/src/app/components/signup/signup.component.ts
+++ client/src/app/components/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  hide: boolean = true;
+  isError: boolean = false;
+  errorMessage: string = "";
+
+  signupForm = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  })
+
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  signup(): void {
+
+  }
+
+  closeAlert() {
+    this.isError = false;
   }
 
 }
