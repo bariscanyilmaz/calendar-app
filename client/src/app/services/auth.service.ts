1721b00c49d730056dd3b9bbcca47a7c6c141083
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2022-03-31 13:39:11 +0300

    configure auth service

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  isLoggedIn:BehaviorSubject<boolean>=new BehaviorSubject<boolean>(false);
+  user:User;
+
   constructor() { }
+
+  login(user:User):Observable<boolean>{
+    return of(true);
+  }
+
+  signup(user:User):Observable<boolean>{
+    return of(true);
+  }
+
+  setLoggedIn(token:string){
+    localStorage.setItem('token',token);
+    this.isLoggedIn.next(true);
+  }
+
+  isAuth():Observable<boolean>{
+    return this.isLoggedIn.asObservable();
+  }
+
 }
